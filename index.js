7ff9afe81fc853f543853fdfa4d20fd736068de7
offluisz <moreiraluis2904@gmail.com>
2026-03-02 21:46:14 -0300

    correcao

--- index.js
+++ index.js
@@ -1,33 +1,24 @@
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const server = express();
 
 server.use(express.json());
-server.use(express.urlencoded({extended: true})); 
+server.use(express.urlencoded({ extended: true }));
+server.set('view engine', 'ejs');
+server.set('views', './views');
 
-server.use(express.static(path.join(__dirname, 'public')));
 
 server.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.render('index');
 });
 
-server.post('/submit', (req,res) =>{
-    const name = req.body.name;
-    const type = req.body.type;
-    res.send(`Name: ${name}, Type: ${type}`);
-    console.log(`Name: ${name}, Type: ${type}`);
+server.post('/submit', (req, res) => {
+  const { name, type } = req.body;
+  res.send(`Name: ${name}, Type: ${type}`);
+  console.log(`Name: ${name}, Type: ${type}`);
 });
 
-if (process.env.NODE_ENV !== 'production') {
-    const PORT = process.env.PORT || 3000;
-    server.listen(PORT,()=>{
-        console.log(`Servidor rodando na porta: ${PORT}`)
-    });
-}
-
-export default server;
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Servidor rodando na porta: ${PORT}`);
+});
